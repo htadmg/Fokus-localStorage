3587e7a6815cc269bb9c5302ade93cb568c50249
Agata Domingues <124289385+htadmg@users.noreply.github.com>
2023-11-08 09:05:23 -0300

    Initial commit

---
+++ script-crud.js
@@ -0,0 +1,45 @@
+const taskListContainer = document.querySelector('.app__section-task-list')
+
+let tarefas = [
+    {
+        descricao: 'Tarefa Concluída', 
+        concluida: true
+    }, 
+    {
+        descricao: 'Tarefa Pendente',
+        concluida: false
+
+    }
+]
+const taksIconSvg = `
+<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
+    fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="12" fill="#FFF" />
+    <path
+        d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+        fill="#01080E" />
+</svg>
+`
+
+function createTask(tarefa){
+    const li = document.createElement('li')
+    li.classList.add('app__section-task-list-item')
+
+    const svgIcon = document.createElement('svg')
+    svgIcon.innerHTML = taksIconSvg
+
+    const paragraph = document.createElement('p')
+    paragraph.classList.add('app__section-task-list-item-description')
+
+    paragraph.textContent = tarefa.descricao
+
+    li.appendChild(svgIcon)
+    li.appendChild(paragraph)
+
+    return li 
+}
+
+tarefas.forEach(task => {
+    const taskItem = createTask(task)
+    taskListContainer.appendChild(taskItem)
+})
